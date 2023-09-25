a52b369121432d228447608cfaad973b195b5c6a
TaichiIwamoto <131168152+TaichiIwamoto@users.noreply.github.com>
2023-09-25 17:58:38 +0900

    MainHome Create

---
+++ home/ramhome.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="./ramhome.css" type="text/css">
+    <title>RAMSNOISE</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-info">
+        <div class="container-fluid">
+            <img src="../resource/RAMSNOISE.png" class="img-fluid">
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="記事・成果物" aria-label="Search">
+                <button class="btn btn-secondary" type="submit">検索</button>
+            </form>
+        </div>
+    </nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand"><small>HOME PAGE</small></a>
+        </div>
+    </nav>
+
+    <a href=""></a>
+
+
+</body>
+
+</html>
