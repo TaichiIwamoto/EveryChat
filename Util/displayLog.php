b7ebef6f2b3e73529325643ecc39641cfa2745a2
TaichiIwamoto <131168152+TaichiIwamoto@users.noreply.github.com>
2023-09-27 00:56:17 +0900

    簡易PHPMyAdmin作成

--- Util/displayLog.php
+++ Util/displayLog.php
@@ -12,12 +12,36 @@ $sql = "CREATE TABLE IF NOT EXISTS user_table"
     . ");";
 $stmt = $pdo->query($sql);
 
+
 echo "TABLE一覧<br>";
 $sql = "SHOW TABLES";
 $stmt = $pdo->query($sql);
 $result = $stmt->fetchAll();
 // var_dump($result);
 foreach ($result as $line) {
-    echo $line[0];
+    echo $line[0] . "<br>";
+    $sql = "SELECT * FROM " . $line[0];
+    $stmt = $pdo->query($sql);
+    $result = $stmt->fetchAll();
+    echo "<table border=1>";
+    foreach ($result as $row) {
+        echo "<tr>";
+        foreach ($row as $key => $value) {
+            if (!is_int($key)) {
+
+                echo "<th>{$key} </th>";
+            }
+        }
+        echo "<tr>";
+        echo "<tr>";
+        foreach ($row as $key => $value) {
+            if (!is_int($key)) {
+                echo "<td>{$value} </td>";
+            }
+        }
+        echo "<tr>";
+    }
+    echo "</table>";
 }
+
 ?>
