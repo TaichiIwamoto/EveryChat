61a2c4d63307e144517aec4027ffec868ef575b4
TaichiIwamoto <taichirui97@gmail.com>
2023-10-04 15:26:39 +0900

    トップヘッダデザイン変更&CSS微調整

--- blog/viewBlog.php
+++ blog/viewBlog.php
@@ -16,14 +16,42 @@ $pdo = connect();
 </head>
 
 <body>
+    <!-- ヘッダ -->
     <nav class="navbar navbar-expand-lg navbar-light bg-info">
-        <div class="container-fluid">
-            <img src="../resource/RAMSNOISE.png" class="img-fluid">
+
+        <img class="navbar-brand" src="../resource/RAMSNOISE.png">
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="../home/ramhome.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Link
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="../user/settingProfile.php">MyProfile</a>
+                        <a class="dropdown-item" href="#">MyBlog</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">ServicesList</a>
+                    </div>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+            </form>
         </div>
-        <a href="../home/ramhome.php">
-            <button class="btn btn-primary">ホームへ</button>
-        </a>
     </nav>
+    <!-- ヘッダ -->
+
 
 
     <?php
